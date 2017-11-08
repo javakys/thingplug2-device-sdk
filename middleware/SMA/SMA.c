a0f1e5b6cde29a1570601117c186345b40177ca3
lesmin-sk <lesmin@sk.com>
2017-11-08 09:42:56 +0900

    update SMA (added random data generator)

--- middleware/SMA/SMA.c
+++ middleware/SMA/SMA.c
@@ -12,48 +12,34 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <time.h>
 #include "SMA.h"
 
 #define SMA_STRCMP(x,y) (strncmp(x,y,strlen(y)) == 0)&&(strlen(x) == strlen(y))
 
-int dummyIdx = 0;
-
 void SMAGetData(char *sensorType, char** output, int *len)
 {
+    srand(time(NULL));
+    int random_num = rand();
+
     if( SMA_STRCMP(sensorType,"batterystate") ) {
         *output = strdup("1");
         *len = strlen(*output);
     } else if( SMA_STRCMP(sensorType, "temp1") ) {
-        if( dummyIdx == 0 ) {
-            *output = strdup("26.26");
+	*output = strdup("XX.XX");
+	sprintf(*output, "%.2f", (random_num%5000+2000)/100.0);
             *len = strlen(*output);
-            dummyIdx = 1;
-        } else {
-            *output = strdup("25.25");
-            *len = strlen(*output);
-        }
     } else if( SMA_STRCMP(sensorType, "humi1") ) {
-        if( dummyIdx == 0 ) {
-            *output = strdup("48");
+	*output = strdup("XX");
+	sprintf(*output, "%2d", (random_num%10+40));
             *len = strlen(*output);
-        } else {
-            *output = strdup("44");
-            *len = strlen(*output);
-        }
     } else if( SMA_STRCMP(sensorType, "light1") ) {
-        if( dummyIdx == 0 ) {
-            *output = strdup("278");
+	*output = strdup("XXX");
+	sprintf(*output, "%3d", (random_num%50+200));
             *len = strlen(*output);
         } else {
-            *output = strdup("267");
-            *len = strlen(*output);
-        }
-    } else {
         *output = NULL;
     }
-    dummyIdx++;
-    dummyIdx%=2;
 }
 
 static int ledStatus = 0;
